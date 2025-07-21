c083072fe57dbdc9a120ca401f13d04a75b30f85
thangameenakshi <thangameenakshi1201@gmail.com>
2025-07-21 19:05:09 +0530

    Add files via upload

---
+++ enumeration.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ enum bool{NO,YES};
+int main()
+{
+    enum point{x=10,y};
+    enum bool a;
+    enum bool b;
+    b=y;
+    a=YES;
+    printf("A: %d",a);
+    printf("\n B: %d",b);
+    return 0;
+}
